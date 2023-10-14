d96cacce00cee1f19068b93521def26c42f0c000
Aditya-Raj-009 <adityasinghrajput2107200@gmail.com>
2023-10-14 23:42:10 +0530

    setup logout option

--- app/src/main/java/com/example/khaata/MainActivity.java
+++ app/src/main/java/com/example/khaata/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.khaata;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.khaata.databinding.ActivityMainBinding;
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getWindow().setFlags(1024,1024);
 
+        setSupportActionBar(binding.toolbar);
+        
         SharedPreferences sharedPreferences = getSharedPreferences("PaymentInfo",MODE_PRIVATE);
         if(sharedPreferences.getString("PayeeName",null)!=null
                 && sharedPreferences.getString("VPA",null)!=null
@@ -38,5 +45,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.logout)
+        {
+            SharedPreferences sharedPreferences =  getSharedPreferences("PaymentInfo",MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("PayeeName",null);
+            editor.putString("VPA",null);
+            editor.putString("Balance",null);
+            editor.commit();
+            startActivity(new Intent(this,PaymentInfoActivity.class));
+            finish();
+        }
+        else {
+
+        }
+        return true;
     }
 }
